3ac979b07bb7044bab21be84c10a14933dae95c1
ramces <ramces2580@gmail.com>
2020-12-29 12:51:17 -0500

    Updates

---
+++ precache-manifest.a4b2c47e68f3aac8967cfdc092d172ad.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b01fbbd7d54a13e0f9bbf242b3e3987c",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "4a7436949433b0e7a614",
+    "url": "/React-Portfolio/static/css/main.e7e2d697.chunk.css"
+  },
+  {
+    "revision": "cca3af06239471f908aa",
+    "url": "/React-Portfolio/static/js/2.dccdd241.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-Portfolio/static/js/2.dccdd241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a7436949433b0e7a614",
+    "url": "/React-Portfolio/static/js/main.40a03f5b.chunk.js"
+  },
+  {
+    "revision": "7c35ff808cd94c4ce20c",
+    "url": "/React-Portfolio/static/js/runtime-main.0ad08de7.js"
+  },
+  {
+    "revision": "19d1d4d5c2b75bf712b8d5009975e5ba",
+    "url": "/React-Portfolio/static/media/Greece-2.19d1d4d5.jpg"
+  },
+  {
+    "revision": "34bdd94f26dbe3bec393b5c6f2ceec28",
+    "url": "/React-Portfolio/static/media/Profile-picture.34bdd94f.jpg"
+  },
+  {
+    "revision": "354ae5f9096b0afc6e6015a14848e060",
+    "url": "/React-Portfolio/static/media/RamcesLogoMint.354ae5f9.jpg"
+  },
+  {
+    "revision": "892debf822a1e8d8e3dab78b1c128b97",
+    "url": "/React-Portfolio/static/media/Resume.892debf8.pdf"
+  },
+  {
+    "revision": "a019b7f6e8ee9fde06400eb40425c7fb",
+    "url": "/React-Portfolio/static/media/Screenshot-Daily-Journal.a019b7f6.jpg"
+  },
+  {
+    "revision": "2445df351ae66d8531b58477b480f613",
+    "url": "/React-Portfolio/static/media/Screenshot-Simon-Game.2445df35.jpg"
+  },
+  {
+    "revision": "b72ff9a698b4c17c9a5e184385c6a6a8",
+    "url": "/React-Portfolio/static/media/Screenshot-ToDoList.b72ff9a6.jpg"
+  },
+  {
+    "revision": "ad3df7473e77454637550184d832239f",
+    "url": "/React-Portfolio/static/media/Techdegree-project-1-screenshot.ad3df747.png"
+  },
+  {
+    "revision": "2850d755d337956e697b995ea22bdf3b",
+    "url": "/React-Portfolio/static/media/Techdegree-project-2-screenshot.2850d755.png"
+  },
+  {
+    "revision": "50cfc9556c33746aefd86d3d100f8871",
+    "url": "/React-Portfolio/static/media/Techdegree-project-3-screenshot.50cfc955.png"
+  },
+  {
+    "revision": "36dd67d84898201bb257d26654bbc55e",
+    "url": "/React-Portfolio/static/media/Techdegree-project-4-screenshot.36dd67d8.png"
+  },
+  {
+    "revision": "40fab099dbcc553218cec1be1c026dba",
+    "url": "/React-Portfolio/static/media/Techdegree-project-6-screenshot.40fab099.png"
+  },
+  {
+    "revision": "89625300c9c6def9c82ec3a6ae7bae55",
+    "url": "/React-Portfolio/static/media/Techdegree-project-7-screenshot.89625300.png"
+  },
+  {
+    "revision": "5736731939f5e302a971598313455a54",
+    "url": "/React-Portfolio/static/media/Techdegree-project-8-screenshot.57367319.png"
+  },
+  {
+    "revision": "612c193310afe4f966cf4eb6e368fc18",
+    "url": "/React-Portfolio/static/media/amazon-clone.612c1933.jpg"
+  },
+  {
+    "revision": "c9b864a6b7e94de37fecf972e56e0ed2",
+    "url": "/React-Portfolio/static/media/express.c9b864a6.png"
+  },
+  {
+    "revision": "e38bf3f9af455f1b72b859811f9061ec",
+    "url": "/React-Portfolio/static/media/javascript.e38bf3f9.png"
+  },
+  {
+    "revision": "3d21b08248dc6ee5c026dcb5a59b53c9",
+    "url": "/React-Portfolio/static/media/mongoDB.3d21b082.png"
+  },
+  {
+    "revision": "3def23f34d00877b5a9947c8694c4649",
+    "url": "/React-Portfolio/static/media/netflix-clone.3def23f3.jpg"
+  },
+  {
+    "revision": "717f5055f0ac8c5d7ad15b7b5e078e86",
+    "url": "/React-Portfolio/static/media/node.717f5055.png"
+  },
+  {
+    "revision": "37ada809fd429ad318df6835c18e90fd",
+    "url": "/React-Portfolio/static/media/react.37ada809.png"
+  }
+]);
